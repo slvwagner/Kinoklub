09b9a2168c666dc06f95d9f82e217d7414da4101
Florian Wagner <slvwagner@gmail.com>
2023-12-19 19:50:16 +0100

    Version 0.4.0 zur Review

--- Erstelle Abrechnung.R
+++ Erstelle Abrechnung.R
@@ -5,8 +5,13 @@
 # V0.4.0
 #############################################################################################################################################
 source("source/read and convert.R")
+#############################################################################################################################################
+
+# Sollen für jede Vorführung eine Abrechnung erstellt werden?
+c_run_single <- T
 
 #############################################################################################################################################
+# Script start
 df_Render <- tibble(Render  = c("html_document","word_document","pdf_document"), 
                     fileExt = c(".html", ".docx", ".pdf"))
 
@@ -53,39 +58,43 @@ paste("Bericht: \nStatistik erstellt")|>
 
 #############################################################################################################################################
 # Erstellen der Berichte für gewählte Vorführungen
-ii <- 1
-for(ii in 1:nrow(df_mapping)){
+ 
 
-  # Einlesen template der Abrechnung
-  c_raw <- readLines("source/Abrechnung.Rmd")
-  c_raw
-
-  # Ändern des Templates mit user eingaben
-  index <- (1:length(c_raw))[c_raw|>str_detect("variablen")]
-  index
-
-  c_raw[(index+1)] <- c_raw[(index+1)]|>str_replace(one_or_more(DGT), paste0(ii))
-  writeLines(c_raw, paste0("source/temp.Rmd"))
-
-  # Render
-  rmarkdown::render(paste0("source/temp.Rmd"),
-                    df_Render$Render,
-                    output_dir = paste0(getwd(), "/output"))
-
-  # Rename the file
-  for (jj in 1:length(df_Render$Render)) {
-    file.rename(from = paste0(getwd(),"/output/temp",df_Render$fileExt[jj]), 
-                to   = paste0(getwd(),"/output/",df_mapping$user_Datum[ii],df_Render$fileExt[jj])
-                )
+if(c_run_single){
+  for(ii in 1:nrow(df_mapping)){
+  
+    # Einlesen template der Abrechnung
+    c_raw <- readLines("source/Abrechnung.Rmd")
+    c_raw
+  
+    # Ändern des Templates mit user eingaben
+    index <- (1:length(c_raw))[c_raw|>str_detect("variablen")]
+    index
+  
+    c_raw[(index+1)] <- c_raw[(index+1)]|>str_replace(one_or_more(DGT), paste0(ii))
+    writeLines(c_raw, paste0("source/temp.Rmd"))
+  
+    # Render
+    rmarkdown::render(paste0("source/temp.Rmd"),
+                      df_Render$Render,
+                      output_dir = paste0(getwd(), "/output"))
+  
+    # Rename the file
+    for (jj in 1:length(df_Render$Render)) {
+      file.rename(from = paste0(getwd(),"/output/temp",df_Render$fileExt[jj]), 
+                  to   = paste0(getwd(),"/output/",df_mapping$user_Datum[ii],df_Render$fileExt[jj])
+                  )
+    }
+  
+    # user interaction
+    print(clc)
+    paste("Bericht: \nAbrechnung vom", df_mapping$user_Datum[ii], "erstellt")|>
+      writeLines()
   }
-
-  # user interaction
-  print(clc)
-  paste("Bericht: \nAbrechnung vom", df_mapping$user_Datum[ii], "erstellt")|>
-    writeLines()
+  remove(c_raw, index,ii,jj)
 }
 
-remove(c_Datum, c_raw, c_suisa, c_verleiherabgaben, index,ii)
+remove(c_Datum, c_suisa, c_verleiherabgaben, c_run_single)
 
 
 
