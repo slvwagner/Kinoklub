9dba6c3ae75d6d68d071db4f15aec7f202905d70
Florian Wagner <53572950+slvwagner@users.noreply.github.com>
2024-01-24 23:48:36 +0100

    Spez preise merge for Kiosk

--- source/read_kiosk.R
+++ source/read_kiosk.R
@@ -24,46 +24,82 @@ c_fileDate <- str_match(c_files,capture(one_or_more(DGT)%R%DOT%R%one_or_more(DGT
 ######################################################################## 
 
 # detect Verkaufarikel in string
-p <- or1(paste0(df_verkaufsartikel$`Artikelname Kassensystem`))
+p1 <- or1(paste0(df_verkaufsartikel$`Artikelname Kassensystem`))
+
+# detect Spez Preise 
+p2 <- or1(paste0("Spez"%R%SPC, 1:4))
+
+# Detect Überschuss Manko 
+p3 <- optional("-") %R% one_or_more(DGT) %R% optional(DOT %R% one_or_more(DGT))
 
 l_extracted <- list()
-
 for (ii in 1:length(l_raw)) {
-  l_extracted[[ii]] <- tibble(Verkaufartikel_string = l_raw[[ii]][str_detect(l_raw[[ii]], p)])
-}
+  l_extracted[[ii]] <- list(Verkaufsartikel = tibble(Verkaufartikel_string = c(l_raw[[ii]][str_detect(l_raw[[ii]], p1)], ## Arikel erfasst in Kassasystem
+                                                        l_raw[[ii]][str_detect(l_raw[[ii]], p2)] ## Spez Arikel
+                                                        )),
+                            `Überschuss / Manko` = l_raw[[ii]][str_detect(l_raw[[ii]], "Manko")]|>str_extract(p3)
+                            )
+  }
 names(l_extracted) <- c_fileDate
-l_extracted
 
-p <- "Manko" 
-for (ii in 1:length(l_raw)) {
-  l_extracted[[ii]]["Überschuss / Manko"] <- l_raw[[ii]][str_detect(l_raw[[ii]], "Manko")]
-}
-
-df_Kioskverkauf <- l_extracted|>
-  lapply(function(x){
-    y <- x$Verkaufartikel_string|>
+l_Kiosk <- l_extracted |>
+  lapply(function(x) {
+    y <- x[[1]]$Verkaufartikel_string |>
       str_split(pattern = "\t", simplify = T)
     colnames(y) <- c("Verkaufsartikel", "Einzelpreis", "Anzahl", "V4", "Betrag")
-    y <- as_tibble(y)|>
-      select(-V4)|>
-      mutate(Einzelpreis = as.numeric(Einzelpreis),
-             Anzahl = as.numeric(Anzahl),
-             Betrag = as.numeric(Betrag))
-    y$`Überschuss / Manko` <- str_extract(x$`Überschuss / Manko`, optional("-")%R%one_or_more(DGT)%R%optional(DOT%R%one_or_more(DGT)))|>
-      as.numeric()
-    return(y)
+    y <- as_tibble(y) |>
+      select(-V4) |>
+      mutate(
+        Einzelpreis = as.numeric(Einzelpreis),
+        Anzahl = as.numeric(Anzahl),
+        Betrag = as.numeric(Betrag)
+      )
+    bind_rows(y,
+         tibble(
+           Verkaufsartikel = "Überschuss / Manko",
+           Einzelpreis = as.numeric(NA),
+           Anzahl = as.numeric(NA), 
+           Betrag = x[[2]]|>as.numeric()
+           )
+         )
   })
 
-df_Kioskverkauf
+l_Kiosk
 
-df_Kioskverkauf <- bind_rows(df_Kioskverkauf, .id = "Datum")|>
+df_Kioskverkauf <- bind_rows(l_Kiosk, .id = "Datum")|>
   mutate(Datum = lubridate::dmy(Datum))
+
+########################################################################
+# Kiosk Spezialpreise einlesen
+Spezialpreisekiosk <- readxl::read_excel("Input/Spezialpreisekiosk.xlsx")|>
+  mutate(Datum = as.Date(Datum))
+
+Spezialpreisekiosk
+
+########################################################################
+# join data
+l_Kiosk <- l_Kiosk |>
+  lapply(function(x) {
+    x |>
+      left_join(
+        Spezialpreisekiosk |>
+          filter(Datum == pull(distinct(
+            Spezialpreisekiosk, Datum
+          ))[ii]),
+        by = c(Verkaufsartikel = "Spezialpreis")
+      ) |>
+      mutate(
+        Verkaufsartikel = if_else(is.na(Datum), Verkaufsartikel, Artikelname),
+        Einzelpreis = Betrag / Anzahl
+      )
+  })
+names(l_Kiosk) <- c_fileDate
+l_Kiosk
+
+df_Kioskverkauf <- l_Kiosk|>
+  bind_rows(.id = "Datum")
 
 df_Kioskverkauf|>
   group_by(Datum)|>
-  reframe(Betrag = sum(Betrag),
-          `Überschuss / Manko` = `Überschuss / Manko`[1])
-
-
-
+  reframe(Betrag = sum(Betrag))
 
