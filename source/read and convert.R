01fe0de326be8c9c971f6da78d804644c2666321
Florian Wagner <53572950+slvwagner@users.noreply.github.com>
2024-02-12 21:03:49 +0100

    initial commit

--- source/read and convert.R
+++ source/read and convert.R
@@ -240,7 +240,7 @@ c_sheets
 
 df_verleiherabgaben <- readxl::read_excel(c_file,c_sheets[1])|>
   mutate(Datum = as.Date(Datum))|>
-  left_join(readxl::read_excel(c_file,c_sheets[2]))
+  left_join(readxl::read_excel(c_file,c_sheets[2]), by = "Verleiher")
 
 df_verleiherabgaben
 
@@ -269,7 +269,7 @@ df_Verleiher_Rechnnung
 df_keine_Rechnnung <- df_Verleiher_Rechnnung|>
   filter(`keine Verleiherrechnung`)
 
-# error handling
+# error handling, keine Verleiherrechnung
 if(nrow(df_keine_Rechnnung)>0) {
   warning(paste0("\nAchtung für die diesen Film gibt es keine Verleiherrechnung: \n",
                    day(df_keine_Rechnnung$Datum),".",month(df_keine_Rechnnung$Datum),".", lubridate::year(df_keine_Rechnnung$Datum), " ",df_keine_Rechnnung$Filmtitel,"\n")
@@ -280,65 +280,117 @@ if(nrow(df_keine_Rechnnung)>0) {
 ########################################################################
 # Gewinn/Verlust Eintitt
 ########################################################################
+df_Kinopreise <- df_Eintritt|>
+  distinct(Platzkategorie,.keep_all = T)|>
+  select(Platzkategorie, Verkaufspreis)
+df_Kinopreise
 
 l_GV <- list()
-ii <- 3
+ii <- 2
 for (ii in 1:length(c_Date)) {
-  c_Besucher <- df_Eintritt|>
+  
+  # Kinoförderer dürfen nicht bei jedem Verleiher als gratis abgerechnet werden
+  c_Kinofoerder_gratis <- df_Eintritt|>
     filter(Datum == c_Date[ii])|>
-    reframe(Besucherzahl = sum(Anzahl))|>
+    distinct(`Kinoförderer gratis?`)|>
+    mutate(`Kinoförderer gratis?` = if_else(`Kinoförderer gratis?` == "ja", T, F))|>
     pull()
-  c_Besucher
-  
-  c_Gratis <- df_Eintritt|>
-    filter(Datum == c_Date[ii], !Zahlend)|>
-    reframe(Gratiseintritte = sum(Anzahl))|>
-    pull()
-  c_Gratis
-  
-  ##Brutto 
-  c_Umsatz <- df_Eintritt|>
-    filter(Datum == c_Date[ii])|>
-    reframe(Umsatz = sum(Umsatz))|>
-    pull()
-  
-  c_suisaabzug <- (distinct(df_Eintritt|>filter(Datum == c_Date[ii]), `SUISA-Vorabzug`)|>
-                     pull())/100
+  c_Kinofoerder_gratis
+
+  if(!c_Kinofoerder_gratis){ # Kinoföderer müssen abgerechnet werden
+    df_temp <- df_Eintritt |>
+      filter(Datum == c_Date[ii], Zahlend) |>
+      bind_rows(
+        df_Eintritt |>
+          filter(Datum == c_Date[ii], Platzkategorie == "Kinoförderer")|>
+          mutate(Abrechnungspreis = df_Kinopreise|>
+                   filter(Platzkategorie == "Ermässigt")|>
+                   select(Verkaufspreis)|>pull()
+                 )
+      )|>
+      mutate(Umsatz = if_else(is.na(Abrechnungspreis), Umsatz, Abrechnungspreis *  Anzahl))|>
+      select(-Abrechnungspreis)
+    df_temp
+    
+    c_Besucher <- df_temp|>
+      reframe(Anzahl = sum(Anzahl))
+    
+    c_Gratis <- df_Eintritt |>
+      filter(Datum == c_Date[ii])|>
+      reframe(Anzahl = sum(Anzahl))|>
+      pull() - c_Besucher
+    
+    ##Brutto
+    c_Umsatz <- df_temp |>
+      filter(Datum == c_Date[ii]) |>
+      reframe(Umsatz = sum(Umsatz)) |>
+      pull()
+    c_Umsatz
+    
+  } else { # Kinoföderer sind gratis
+    
+    c_Besucher <- df_Eintritt |>
+      filter(Datum == c_Date[ii]) |>
+      reframe(Besucherzahl = sum(Anzahl)) |>
+      pull()
+    c_Besucher
+    
+    c_Gratis <- df_Eintritt |>
+      filter(Datum == c_Date[ii], !Zahlend) |>
+      reframe(Gratiseintritte = sum(Anzahl)) |>
+      pull()
+    c_Gratis
+    
+    ##Brutto
+    c_Umsatz <- df_Eintritt |>
+      filter(Datum == c_Date[ii]) |>
+      reframe(Umsatz = sum(Umsatz)) |>
+      pull()
+    c_Umsatz
+  } 
+
+  c_suisaabzug <- (distinct(df_Eintritt |> 
+                filter(Datum == c_Date[ii]), `SUISA-Vorabzug`) |>
+       pull()) / 100
   c_suisaabzug
   
   ## Netto 3
-  c_Netto3 <- c_Umsatz-round5Rappen(c_Umsatz*c_suisaabzug)
+  c_Netto3 <- c_Umsatz - round5Rappen(c_Umsatz * c_suisaabzug)
   c_Netto3
   
   ## Prozent von Netto 3 bezahlt werden müssen
-  c_verleiherabzug <- (distinct(df_Eintritt|>filter(Datum == c_Date[ii]), 
-                               `Abzug [%]`)|>pull())/100
+  c_verleiherabzug <-
+    (distinct(df_Eintritt |> 
+                filter(Datum == c_Date[ii]),
+              `Abzug [%]`) |> pull()) / 100
+  c_verleiherabzug
   
-  c_Verleiherrechnung <- df_Verleiher_Rechnnung |>
-        filter(Datum == c_Date[ii]) |>
-        select(Betrag) |>
-        pull()
+  c_Verleiherrechnung <- c_Netto3*c_verleiherabzug
   
   c_Verleiherrechnung
   
-  # Mehrwertsteuer auf der Verleiherrechnung 
-  c_MWST_Abzug <- round5Rappen(c_Verleiherrechnung-(c_Verleiherrechnung/(1+(c_MWST/100))))
-  c_MWST_Abzug
+  ### Wenn die ababbe von Netto 3 kleiner 150CHF ist muss minimal 150 abgegeben werden
+  if (c_Verleiherrechnung < 150) {
+    c_Verleiherrechnung <- 150
+  }
+  c_MWST_Abzug <- round5Rappen(c_Verleiherrechnung) * (c_MWST / 100)
+  
+ 
   
   # Berechnung der Abgaben
-  if(is.na(c_MWST_Abzug)) { # Wemm keine Verleiherrechnung vorhanden ist muss die MWST der Verleiherrechnung berechnet werden.
+  if (is.na(c_MWST_Abzug)) {
+    # Wemm keine Verleiherrechnung vorhanden ist muss die MWST der Verleiherrechnung berechnet werden.
     
-    c_Verleiherrechnung <- round5Rappen(c_Netto3*c_verleiherabzug)
-    c_Verleiherrechnung
+    # Mehrwertsteuer auf der Verleiherrechnung
+    c_MWST_Abzug <-
+      round5Rappen(c_Verleiherrechnung - (c_Verleiherrechnung / (1 + (c_MWST /
+                                                                        100))))
+    c_MWST_Abzug
     
-    ### Wenn die ababbe von Netto 3 kleiner 150CHF ist muss minimal 150 abgegeben werden
-    if(c_verleiherabzug < 150) c_Verleiherrechnung <- 150
-    
-    c_MWST_Abzug <- round5Rappen(c_Verleiherrechnung)*(c_MWST/100)
-    
-    }
+  }
   c_MWST_Abzug
   c_Verleiherrechnung
+  
   
   # Error handling Verleiherabgaben
   if(is.na(c_verleiherabzug)) {
@@ -381,9 +433,10 @@ l_GV
 
 df_GV_Eintritt <- l_GV|>
   bind_rows()
+df_GV_Eintritt
 
 names(df_GV_Eintritt)
-df_GV_Eintritt$`Gewinn/Verlust [CHF]`
+df_GV_Eintritt
 
 
 ########################################################################
