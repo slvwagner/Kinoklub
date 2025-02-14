9ed7ad57259cbc3186e7265c4e42f0d78522e1ca
Florian Wagner <53572950+slvwagner@users.noreply.github.com>
2025-02-14 14:19:29 +0100

    XAMPP Server PHP to deploy app to internal network

---
+++ source/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kinoklub</title>
+    <style>
+        body {
+            background-color: #21b4cc;
+            margin: 0;
+            padding: 0;
+        }
+        iframe {
+            width: 100%;
+            height: 905px;
+            border: none;
+        }
+    </style>
+</head>
+<body>
+    <iframe src="http://192.168.0.163:5000/"></iframe>
+</body>
+</html>
