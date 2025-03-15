e92e29af0cf9c65dea166bb25e5c62e2269c02ee
Florian Wagner <53572950+slvwagner@users.noreply.github.com>
2025-03-15 08:51:47 +0100

    mc

--- edit_input_data.R
+++ edit_input_data.R
@@ -137,7 +137,7 @@ ui <- function(){
                         ),
     # Ensure jQuery UI is available for dragable tool box
     # includeScript("https://code.jquery.com/ui/1.12.1/jquery-ui.js"),
-    includeScript("source/1.12.1_jquery-ui.js"),
+    includeScript("source/JS/1.12.1_jquery-ui.js"),
     tags$head(
       tags$style(HTML("
         #floating-panel {
