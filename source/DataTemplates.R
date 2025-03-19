377a87cd316fb729ebaa1af3479763912944e167
Florian Wagner <53572950+slvwagner@users.noreply.github.com>
2025-03-19 16:21:27 +0100

    new data template

--- source/DataTemplates.R
+++ source/DataTemplates.R
@@ -1,20 +1,17 @@
 # Data templates
-library(lubridate)
-library(hms)
+
+library(tidyverse)
+
+# read in data templates (for data type conversion)
+l_data <- readRDS("Input/Data.Rds")
+
 
 l_template <- l_data |> 
   lapply(function(x) {
     x |> 
-      slice(1) |> 
-      mutate(
-        across(where(is.character), ~NA_character_),
-        across(where(is.double), ~NA_real_),
-        across(where(is.integer), ~NA_integer_),
-        across(where(is.factor), ~factor(NA, levels = levels(.))),
-        across(where(lubridate::is.Date), ~as.Date(NA)),
-        across(where(lubridate::is.POSIXct), ~as.POSIXct(NA, origin = "1970-01-01")),
-        across(where(hms::is.hms), ~hms::as_hms(NA))
-      )
+      slice(1)
   })
+
+l_template
 
 saveRDS(l_template,file = "Input/template.Rds")
