1789dd702ba778491959b4d7408f0e15059c5020
Florian Wagner <53572950+slvwagner@users.noreply.github.com>
2025-04-16 15:51:57 +0200

    Backup reworked

--- source/SQL/SQL_backup_data.R
+++ source/SQL/SQL_backup_data.R
@@ -5,10 +5,21 @@ source("source/SQL/SQL_Functions.R")
 pw <- Sys.getenv("DB_PASSWORD_KINOKLUB")
 con <- DB_connect(pw, "ch367079_flo")
 l_data <- DB_backup_DB(con)
-
-dbExecute(con, sprintf("DROP TABLE IF EXISTS `%s`", "df_Eintritt"))
-
 saveRDS(l_data, "Backup/Data.Rds")
+
+# update template
+l_template <- l_data|>
+  lapply(function(df){
+    df|>
+      slice(1)
+  })
+
+# Update
+saveRDS(l_template,"source/SQL/template.Rds")
+saveRDS(l_data, c_file)
+
+
+# dbExecute(con, sprintf("DROP TABLE IF EXISTS `%s`", "df_Eintritt"))
 
 ###################################################
 # Disconnect from DB
