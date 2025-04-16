7d505048946b8cc22dc4e330054fb186d9f631d8
Florian Wagner <53572950+slvwagner@users.noreply.github.com>
2025-04-16 15:47:57 +0200

    Backup

--- source/SQL/SQL_upload_data.R
+++ source/SQL/SQL_upload_data.R
@@ -2,17 +2,9 @@
 source("source/SQL/SQL_Functions.R")
 
 #Load the data
-c_file <- "Input/Data.Rds"
-if(file.exists(c_file)){
-  l_data <- readRDS(c_file)
-  c_backup_number <- length(list.files(path = "Input/backup", pattern = "backup"))
-  if(!dir.exists("Input/backup")) dir.create("Input/backup")
-  saveRDS(l_data, paste0("Input/backup/Data_backup",c_backup_number + 1,".Rds")) # Save the updated list to the file
-}else{ # or load template date
-  c_file <- "Input/template.Rds"
-  l_data <- readRDS(c_file)
-  c_file <- "Input/Data.Rds"
-}
+c_file <- "Backup/Data.Rds"
+l_data <- readRDS(c_file)
+
 
 # # Change columns
 # l_data$Programm <- l_data$Programm|>
