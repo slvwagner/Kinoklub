b0efa0a13d5704005c190e6149360e288edb3cdd
Florian Wagner <53572950+slvwagner@users.noreply.github.com>
2025-04-17 23:49:10 +0200

    crate Filmvorschlag

---
+++ source/Filmvorschlag.R
@@ -0,0 +1,81 @@
+library(tidyverse)
+library(rebus)
+library(lubridate)
+
+c_filePath <- "Input/advance tickets/"
+
+Shows <- read_delim(paste0(c_filePath,"Shows.txt"), 
+                    delim = "\t", escape_double = FALSE, 
+                    col_types = cols(Tag = col_date(format = "%Y-%m-%d"), 
+                                     Anfang = col_time(format = "%H:%M"), 
+                                     Ende = col_skip(), Saal = col_skip(), 
+                                     Version = col_skip(), Alter = col_skip()), 
+                    trim_ws = TRUE, skip = 7)|>
+  suppressWarnings()
+Shows <- Shows|>
+  rename(Zeit = Anfang,
+         Filmtitel = Titel,
+         Datum = Tag)
+Shows
+
+df_files <- tibble(
+  Eintritt = list.files(c_filePath, pattern = "trit", full.names = TRUE),
+  Kiosk = list.files(c_filePath, pattern = "iosk", full.names = TRUE),
+  Suisanummer = "",
+  Datum = "",
+  Filmtitel = ""
+)
+df_files
+
+ii <- 1
+for (ii in 1:nrow(df_files)) {
+  c_raw <-readLines(df_files$Eintritt[ii])|>
+    suppressWarnings()
+  c_raw
+  
+  c_suisa <- str_extract(c_raw, pattern = WRD%R%WRD%R%WRD%R%WRD%R%DOT%R%WRD%R%WRD%R%WRD)
+  c_suisa <- c_suisa[!is.na(c_suisa)]
+  df_files[ii,"Suisanummer"] <- c_suisa
+  
+  c_Datum <- str_extract(c_raw, pattern = "\t"%R%DGT%R%DGT%R%DOT%R%DGT%R%DGT%R%DOT%R%DGT%R%DGT%R%DGT%R%DGT)|>
+    str_remove("\t")
+  c_Datum
+  c_Datum <- c_Datum[!is.na(c_Datum)]
+  df_files[ii,"Datum"] <- c_Datum
+  
+  # Pattern: 4 digits, a dot, 3 digits, tab, capture the rest
+  p <- "\\w{4}\\.\\w{3}\\t(.*)"
+  
+  c_Film <- str_match(c_raw, p)[,2]
+  c_Film <- c_Film[!is.na(c_Film)]
+  df_files[ii,"Filmtitel"] <- c_Film[!is.na(c_Film)]
+  
+}
+df_files <- df_files|>
+  mutate(Datum = dmy(Datum))
+df_files
+
+source("source/functions.R")
+
+l_search <- df_files$Suisanummer|>
+  lapply(search_procinema_by_suisa)
+names(l_search) <- df_files$Suisanummer
+l_search
+
+l_details <- l_search|>
+  lapply(function(x){
+    if(is.null(x)) return(NULL)
+    else film_details(x$link)
+  })
+
+df_Filmdetails <- l_details|>
+  bind_rows(.id = "Suisanummer")
+
+df_Filmdetails <- df_Filmdetails|>
+  rename(Inhalt = synopsis,
+         Filmtitel = title
+         )|>
+  mutate(across(contains("release"), 
+                ~ as.Date(., format = "%d.%m.%Y")))
+
+print(df_Filmdetails)
