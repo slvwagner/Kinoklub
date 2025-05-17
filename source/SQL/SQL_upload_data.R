05660ac2a92d899b5e8b994269a300503486792a
Florian Wagner <53572950+slvwagner@users.noreply.github.com>
2025-05-17 14:59:53 +0200

    use lastest backup to recover database

--- source/SQL/SQL_upload_data.R
+++ source/SQL/SQL_upload_data.R
@@ -2,11 +2,9 @@
 source("source/SQL/SQL_Functions.R")
 
 #Load the data
-c_file <- "Backup/Data.Rds"
+c_files <- list.files(path = "Backup", full.names = TRUE)
+c_file <- c_files[length(c_files)]
 l_data <- readRDS(c_file)
-
-l_data$Programm
-l_data$Einsatzplan
 
 # Data base user password from system variables 
 pw <- Sys.getenv("DB_PASSWORD_KINOKLUB")
