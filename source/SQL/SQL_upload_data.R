b048b708bfab500f56df9e3d19ee97376c15ab30
Florian Wagner <53572950+slvwagner@users.noreply.github.com>
2025-05-29 12:49:06 +0200

    Bug fix: Use latest backup to upload to database

--- source/SQL/SQL_upload_data.R
+++ source/SQL/SQL_upload_data.R
@@ -3,8 +3,22 @@ source("source/SQL/SQL_Functions.R")
 
 #Load the data
 c_files <- list.files(path = "Backup", full.names = TRUE)
-c_file <- c_files[length(c_files)]
-l_data <- readRDS(c_file)
+
+df_temp <- tibble(files = c_files)
+
+# library(rebus)
+# p <- capture(one_or_more(DGT))%R%DOT%R%"Rds"
+p <- "([\\d]+)\\.Rds"
+
+# find latest backup
+df_temp <- df_temp|>
+  mutate(ID = str_match(c_files, p)[,2]|>
+           as.integer()
+         )|>
+  filter(ID == max(ID, na.rm = TRUE))
+df_temp
+
+l_data <- readRDS(df_temp$files)
 
 ## Data base credentials from system variables ####
 DB_host <- Sys.getenv("DB_host")
