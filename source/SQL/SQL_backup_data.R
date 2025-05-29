49850bb7693e862461b66664c5570e671bcb6d29
Florian Wagner <53572950+slvwagner@users.noreply.github.com>
2025-05-29 14:20:02 +0200

    Database backup

--- source/SQL/SQL_backup_data.R
+++ source/SQL/SQL_backup_data.R
@@ -12,25 +12,22 @@ con <- DB_connect(DB_host, DB_name, DB_user, DB_pw)
 
 # read template
 l_template <- readRDS("source/SQL/template.RDS")
+names(l_template$Programm)
 
+# Backup
+l_data <- DB_backup_DB(con)
 
-# # update template
-# l_template$MWST <- l_template$MWST|>
-#   mutate(Abrechnungsjahr = 2023L)
-# 
-# # Backup
-# l_data <- DB_backup_DB(con)
-# 
-# l_template$Filmvorschlag <- l_data$Filmvorschlag|>
-#   mutate(`Start-Datum` = as.Date(`Start-Datum`),
-#          Verleiher = as.factor(Verleiher))|>
+# l_template$Programm <- l_data$Programm|>
+#   mutate(Datum = as.Date(Datum),
+#          Zeit = parse_time(Zeit),
+#          Verleiher = factor(Verleiher),
+#          `Verleiher Angefragt?` = as.factor(`Verleiher Angefragt?`)
+#          )|>
 #   slice(1)
-# 
-# l_data <- l_data|>
-#   convert_DB_to_R(l_template)
-# 
-# l_data$Filmvorschlag
-# 
+
+l_data <- l_data|>
+  convert_DB_to_R(l_template)
+
 # saveRDS(l_template, "source/SQL/template.Rds")
 
 ################################
