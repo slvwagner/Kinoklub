c3107d08ecb99d1c157f9a60e74224ad648b0cf3
Florian Wagner <53572950+slvwagner@users.noreply.github.com>
2025-05-31 21:46:21 +0200

    delete file if report is created

--- GUI.R
+++ GUI.R
@@ -288,10 +288,9 @@ server <- function(input, output, session) {
           data_env
         )
       }, .options = furrr_options(seed = NULL))
-      
-      # remove temp files RMD files
-      file.remove(df_mapping$fileName_RMD)
     }
+    # remove temp files RMD files
+    file.remove(df_mapping$fileName_RMD)
   }
   
   ### Erstellen der Verleiherabrechnung pro Filmvorführung ####
