993ca7c09444f9e34cbc022aa4bdc2f834e0db5b
Florian Wagner <53572950+slvwagner@users.noreply.github.com>
2025-06-03 21:00:04 +0200

    Date base backup reworked

--- source/SQL/SQL_backup_data.R
+++ source/SQL/SQL_backup_data.R
@@ -13,8 +13,11 @@ con <- DB_connect(DB_host, DB_name, DB_user, DB_pw)
 # read template
 l_template <- readRDS("source/SQL/template.RDS")
 
+l_template$Einsatzplan
+
 
 # # change template
+# l_template$Einsatzplan
 # l_template$Programm
 # l_template$Programm <- l_template$Programm|>
 #   mutate(`Link to Event ID` = factor(`Link to Event ID`))
@@ -30,21 +33,12 @@ l_template <- readRDS("source/SQL/template.RDS")
 l_data <- DB_backup_DB(con)
 l_data <- convert_DB_to_R(l_data, l_template)
 
-# Update Programm and Einsatzplan
-l_data$Einsatzplan <- l_data$Programm|>
-  select(`Event ID`, Suisanummer, Filmtitel, Datum, Zeit, `Verleiher Angefragt?`)|>
-  left_join(l_data$Einsatzplan|>
-              select(-(2:6)
-                     ),
-            by = join_by(`Event ID`)
-            )|>
-  arrange(Datum)
-
 list.files(path = "Backup")
 
 saveRDS(l_data, paste0("Backup/Data",length(list.files(path = "Backup")) + 1L,".Rds"))
 
-# dbExecute(con, sprintf("DROP TABLE IF EXISTS `%s`", "Verleiherabgaben"))
+l_data$Einsatzplan
+
 
 ###################################################
 # Disconnect from DB
