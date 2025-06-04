ec24ffb74742b2907c6f9054a05702899f9cda96
Florian Wagner <53572950+slvwagner@users.noreply.github.com>
2025-06-04 20:40:13 +0200

    mc

--- GUI.R
+++ GUI.R
@@ -2137,10 +2137,10 @@ server <- function(input, output, session) {
     if(os_name == "Windows"){
       shell.exec(normalizePath("output"))
     } else if (os_name == "Darwin"){ # MAC OS
-      path <- "~/output"
+      path <- "/output"
       system(paste("open", shQuote(path)))
     } else if (os_name == "Linux"){
-      path <- "~/output"
+      path <- "/output"
       system(paste("xdg-open", shQuote(path)))
     } else {
       stop("Operating system: ", os_name, " was not implemented for shell actions")
