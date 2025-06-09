3c6779de5cff5ea764330fdeecd8f61dc501d742
Florian Wagner <53572950+slvwagner@users.noreply.github.com>
2025-06-09 17:09:43 +0200

    mc

--- source/OS_support/Kinoklub.bat
+++ source/OS_support/Kinoklub.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Starting Kinoklub Shiny app...
-"C:\Program Files\R\R-4.5.0\bin\Rscript.exe" "Z:\R Packages\Kinoklub\Start_GUI.R"
+"C:\Program Files\R\R-4.5.0\bin\Rscript.exe" "Z:\R Packages\Kinoklub\Start_Input_data_edit.R"
 echo.
 echo App has exited. Press any key to close.
 pause
