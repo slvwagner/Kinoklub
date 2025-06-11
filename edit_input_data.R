0b9d40195873875e92bd98a20d4cf6732ef3a397
Florian Wagner <53572950+slvwagner@users.noreply.github.com>
2025-06-11 14:31:04 +0200

    mc

--- edit_input_data.R
+++ edit_input_data.R
@@ -1242,8 +1242,8 @@ server <- function(input, output, session) {
       df_temp <- DB_get_table(input$dataset, DB_con()) |>
         convert_to_template_types(l_template[[input$dataset]])
       
-      # # Debug print
-      # print(df_temp[order(pull(df_temp[,1]), decreasing = TRUE),])
+      # Debug print
+      print(df_temp[order(pull(df_temp[,1]), decreasing = TRUE),])
       
       # get data
       l_temp <- l_data()
