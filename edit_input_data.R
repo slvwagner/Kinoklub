1a941832d7c66f8479c1889214c941c9ae9f24ee
Florian Wagner <slvwagner@gmail.com>
2025-06-12 17:25:58 +0200

    test new feature: Einnahmen mit Modal

--- edit_input_data.R
+++ edit_input_data.R
@@ -444,6 +444,40 @@ server <- function(input, output, session) {
         shiny::downloadButton("table_export", "Tabelle herunterladen")
       )
     } 
+    #### Ausgaben ####
+    else if (data_set_select == "Einnahmen"){
+      tags$div(
+        id = "floating-panel",
+        tags$div(id = "floating-panel-header", 
+                 "Werkzeuge",
+                 span(class = "toggle-panel", id = "togglePanel", icon("minus"))
+        ),
+        div(class = "custom-select",
+            selectizeInput("dataset", "Datensatz zum Editieren", selected = data_set_select, choices = names(l_data_input)
+            )
+        ),
+        # Function selection
+        shiny::radioButtons(inputId =  "data_selection", label ="Welche Dateien sollen editiert werden?",
+                            choices = choices, selected = choices[choices_select]
+        ),
+        shiny::tags$hr(),
+        actionButton("add_row_einnahmen", "Eintrag hinzufügen", class = "btn-info"),
+        actionButton("edit_row", "Zeile editieren", class = "btn-info"),
+        shiny::tags$hr(),
+        # actionButton("add_row_top", "Zeile oben hinzufügen", class = "btn-info"),
+        # actionButton("add_row_bottom", "Zeile unten hinzufügen", class = "btn-info"),
+        actionButton("duplicate_row", "Zeile duplizieren", class = "btn-info"),
+        shiny::tags$hr(),
+        actionButton("archive_row", "Filmtitel ändern", class = "btn-success"),
+        shiny::tags$hr(),
+        actionButton("delete_row", "Zeile Löschen", class = "btn-danger"),
+        shiny::tags$hr(),
+        actionButton("check_unique", "Prüfen", class = "btn-success"),
+        shiny::tags$hr(),
+        actionButton("get_email", "Email-Verteiler", class = "btn-info"),
+        shiny::downloadButton("table_export", "Tabelle herunterladen")
+      )
+    } 
     ### Spezialpreisekiosk ####
     else if (data_set_select == "Spezialpreisekiosk") {
       tags$div(
@@ -2170,6 +2204,56 @@ server <- function(input, output, session) {
     last_selected_page(1)
     
   })
+  
+  
+  ###  add row on top of selected row ####
+  observeEvent(input$add_row_einnahmen, {
+    # check DB connection
+    if (!dbIsValid(DB_con())) {
+      showNotification(paste("Database connection got lost, try to reconnect."), type = "warning")
+      DB_connect(DB_host(), DB_name(), DB_user(), DB_pw())|>
+        DB_con()
+      showNotification(paste("Database connection recovered"), type = "message")
+    }
+    
+    # Create an empty row
+    new_row <- current_data()[1, ] |> 
+      mutate(across(everything(), ~ NA))|>
+      convert_to_template_types(l_template[[lastEdited_data_set_name()]])
+    new_row[1,1] <- max(current_data()[,1]) + 1L
+    
+    tbl(DB_con(), "Kategorie", downlaod = FALSE)|>
+      select(Auswahl)
+    
+    # add row on top
+    updated_data <-
+      bind_rows(new_row, 
+                current_data()
+      )|>
+      convert_to_template_types(l_template[[lastEdited_data_set_name()]])
+    
+    # updata SQL DB and current data 
+    DB_add_row(DB_con(), lastEdited_data_set_name(), new_row)
+    
+    # Update the list
+    l_temp <- l_data()
+    l_temp[[lastEdited_data_set_name()]] <- DB_get_table(lastEdited_data_set_name(), DB_con())|>
+      convert_to_template_types(l_template[[lastEdited_data_set_name()]])
+    # update all data
+    l_data(l_temp)
+    # update choices
+    update_choices(l_data())|>
+      column_choices()
+    
+    # update to render 
+    current_data(updated_data)
+    
+    #### select last edited row and page ####
+    last_selected_row(1)
+    last_selected_page(1)
+    
+  })
+    
   
   ###  add row on top of selected row ####
   observeEvent(input$add_row_top, {
