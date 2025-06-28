d509ea01722f813f5c0900c65df7a5a11d5ff887
Florian Wagner <53572950+slvwagner@users.noreply.github.com>
2025-06-28 19:14:25 +0200

    round to 5 rappen reworked

--- source/functions.R
+++ source/functions.R
@@ -4,27 +4,8 @@ library(tidyverse)
 library(purrr)
 
 # spez. Round for Swiss currency "CHF" ####
-round5Rappen <- function(zahl) {
-  result <- lapply(zahl, function(zahl){
-    if(is.na(zahl)){
-      return(NA)
-    }
-    else{
-      x <- round(zahl-as.integer(zahl*10)/10,6)
-      if(x>=0.075){
-        return((as.integer(zahl*10)/10)+0.1)
-      }else {
-        if(x>=0.025){
-          return((as.integer(zahl*10)/10)+0.05)
-        }else{
-          return((as.integer(zahl*10)/10)+0.0)
-        }
-      }  
-    }
-  })
-  result|>
-    unlist()|>
-    round(2)
+round5Rappen <- function(x) {
+  round(x * 20) / 20
 }
 
 # variable is present in global environment ####
