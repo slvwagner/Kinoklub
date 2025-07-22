99c050427451639afa3aa0b9dfb990761fb1d73c
Florian Wagner <53572950+slvwagner@users.noreply.github.com>
2025-07-22 15:45:52 +0200

    Application with Icon support for windows

--- source/OS_support/create bat to startup app.R
+++ source/OS_support/create bat to startup app.R
@@ -1,3 +1,65 @@
+create_shortcut <- function(
+    target_bat,
+    shortcut_path,
+    icon_path = NULL,
+    working_dir = NULL,
+    description = NULL
+) {
+  # Ensure all paths are normalized
+  target_bat <- normalizePath(target_bat, winslash = "\\", mustWork = TRUE)
+  shortcut_path <- normalizePath(shortcut_path, winslash = "\\", mustWork = FALSE)
+  
+  if (!grepl("\\.lnk$", shortcut_path, ignore.case = TRUE)) {
+    shortcut_path <- paste0(shortcut_path, ".lnk")
+  }
+  
+  icon_line <- if (!is.null(icon_path)) {
+    icon_path <- normalizePath(icon_path, winslash = "\\", mustWork = TRUE)
+    paste0("shortcut.IconLocation = \"", icon_path, "\"")
+  } else {
+    ""
+  }
+  
+  working_dir_line <- if (!is.null(working_dir)) {
+    working_dir <- normalizePath(working_dir, winslash = "\\", mustWork = TRUE)
+    paste0("shortcut.WorkingDirectory = \"", working_dir, "\"")
+  } else {
+    ""
+  }
+  
+  description_line <- if (!is.null(description)) {
+    paste0("shortcut.Description = \"", description, "\"")
+  } else {
+    ""
+  }
+  
+  vbs_script <- paste(
+    "Set WshShell = CreateObject(\"WScript.Shell\")",
+    paste0("Set shortcut = WshShell.CreateShortcut(\"", shortcut_path, "\")"),
+    paste0("shortcut.TargetPath = \"", target_bat, "\""),
+    icon_line,
+    working_dir_line,
+    description_line,
+    "shortcut.Save",
+    sep = "\n"
+  )
+  
+  # Write VBS script
+  vbs_file <- tempfile(fileext = ".vbs")
+  writeLines(vbs_script, vbs_file)
+  
+  # Run the VBS script
+  system(paste("cscript //nologo", shQuote(vbs_file)))
+  
+  # Cleanup
+  unlink(vbs_file)
+}
+
+
+
+
+
+
 # crate bat 
 
 r_path <- function(x) {
@@ -44,7 +106,17 @@ c_raw
 c_raw[5] <- paste0("\"",r_exe,"\""," ","\"", r_file, "\"")
 c_raw
 
+# Write bat file
 writeLines(c_raw, "source/OS_support/Kinoklub_input.bat")
+
+# create shortcut
+create_shortcut(
+  target_bat = paste0(getwd(),"/source/OS_support/Kinoklub_input.bat"),
+  shortcut_path = paste0(getwd(),"/source/OS_support/Kinoklub input"),
+  icon_path = paste0(getwd(),"/source/OS_support/wagnius.ico"),
+  working_dir = getwd(),
+  description = "Kinoklub Input Tabellen"
+)
 message("Die Datei: ",getwd(),"`source/OS_support/Kinoklub_input.bat` wurde erstellt.")
 
 
@@ -58,7 +130,19 @@ c_raw
 c_raw[5] <- paste0("\"",r_exe,"\""," ","\"", r_file, "\"")
 c_raw
 
+# Write bat file
 writeLines(c_raw, "source/OS_support/Kinoklub_GUI.bat")
+
+# create shortcut
+create_shortcut(
+  target_bat = paste0(getwd(),"/source/OS_support/Kinoklub_GUI.bat"),
+  shortcut_path = paste0(getwd(),"/source/OS_support/Kinoklub GUI"),
+  icon_path = paste0(getwd(),"/source/OS_support/wagnius.ico"),
+  working_dir = getwd(),
+  description = "Kinoklub GUI"
+)
+
 message("Die Datei: ",getwd(),"`source/OS_support/Kinoklub_GUI.bat` wurde erstellt.")
+
 
 
