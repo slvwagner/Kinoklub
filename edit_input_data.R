dba9d6a7994fda8c626c7b9bf12899898111bfc7
Florian Wagner <53572950+slvwagner@users.noreply.github.com>
2025-10-04 14:30:54 +0200

    mc

--- edit_input_data.R
+++ edit_input_data.R
@@ -1956,7 +1956,7 @@ server <- function(input, output, session) {
   })
   
   ## Render modal table ####
-  output$modal_table <- DT::renderDT()({
+  output$modal_table <- DT::renderDT({
     req(df_temp_to_render())  
     datatable(df_temp_to_render(), 
               rownames = FALSE,
@@ -1971,7 +1971,7 @@ server <- function(input, output, session) {
   })
   
   ## Render modal table 2 ####
-  output$modal_table_2 <- DT::renderDT()({
+  output$modal_table_2 <- DT::renderDT({
     req(temp_01())  
     datatable(temp_01(), 
               rownames = FALSE,
